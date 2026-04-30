33afa344ef5b1a35ebc6e07cd0c37995012d4482
felipexpinho <felipe_pinho8@hotmail.com>
2026-04-29 22:07:53 -0300

    update: multiple texts in either pt-br and en-us

--- src/constants/translations.py
+++ src/constants/translations.py
@@ -6,7 +6,15 @@ TRANSLATIONS = {
                 "label": "Pick a date",
                 "description": "The chosen date will be used for curve analysis"
             },
-            "data_select": "Select data type", # TODO: Adjust here
+            "data_select": "Data type",
+            "interpolation_select": {
+                "label": "Interpolation Type",
+                "values": {
+                    "original": "Original",
+                    "refined": "Refined",
+                    "to_zero": "Gaps to 0",
+                }
+            },
             "load_button": "Load Data",
             "smooth_window": "Smooth Window",
             "iteration_quantity": "Iteration Quantity",
@@ -23,19 +31,28 @@ TRANSLATIONS = {
             "results_tab": {
                 "results": "Results",
                 "background_curve": "Subtraction of Background Curve",
+                "day": "Complete Curve",
             },
             "download_select": {
                 "label": "Choose a file extension",
                 "values": {
-                    "pdf": "Report (.pdf)",
-                    "csv": "Data (.csv)",
+                    "db": "Database (.db + .csv)",
+                    "temp": "Save for validation",
+                    # "pdf": "Report (.pdf)",
+                    # "csv": "Data (.csv)",
                 }
             },
-            "download_button": "Download data",
+            "download_button": "Save data",
         },
         "logs": {
             "possible_flare": "Possible Flare",
-            "possible_problem": "Possible Problem"
+            "possible_problem": "Possible Problem",
+            "start": "Start",
+            "end": "End",
+            "curve": "Curve",
+            "offset": "Offset",
+            "median": "Median",
+            "add_log_label": "Add new event",
         },
         "graphs": {
             "intensity": "Intensity",
@@ -43,6 +60,31 @@ TRANSLATIONS = {
             "normalized": "Normalized",
             "time": "Time",
             "curve": "Curve",
+            "smoothed": "Smoothed",
+            "original": "Original",
+            "no_data_found": "No data available",
+        },
+        "notifications": {
+            "date_not_found": {
+                "title": "Date not found",
+                "message": "The selected date does not have available data. Please choose another date."
+            },
+            "not_enough_curves": {
+                "title": "Not enough curves",
+                "message": "The selected date does not have enough data in nearby days."
+            }
+        },
+        "validation": {
+            "search_data_button": "Search Data",
+            "load_flares_button": "Load Flares",
+            "goes_xray_data": "GOES X-Ray Data",
+            "stations": {
+                "palehua": "Palehua - RSTN",
+                "sagamore_hill": "Sagamore Hill - RSTN",
+                "san_vito": "San Vito - RSTN",
+                "learmonth": "Learmonth - RSTN",
+                "nobeyama": "Nobeyama - NoRP"
+            }
         }
     },
     "pt": {
@@ -52,7 +94,15 @@ TRANSLATIONS = {
                 "label": "Escolha uma data",
                 "description": "A data escolhida será utilizada para análise da curva"
             },
-            "data_select": "Selecione o tipo de dado", # TODO: Adjust here
+            "data_select": "Tipo de dado",
+            "interpolation_select": {
+                "label": "Tipo de Interpolação",
+                "values": {
+                    "original": "Original",
+                    "refined": "Refinada",
+                    "to_zero": "Gaps para 0",
+                }
+            },
             "load_button": "Carregar Dados",
             "smooth_window": "Janela de Suavização",
             "iteration_quantity": "Quantidade de Iterações",
@@ -69,19 +119,28 @@ TRANSLATIONS = {
             "results_tab": {
                 "results": "Resultados",
                 "background_curve": "Subtração da Curva Background",
+                "day": "Curva Completa",
             },
             "download_select": {
                 "label": "Escolha uma extensão",
                 "values": {
-                    "pdf": "Relatório (.pdf)",
-                    "csv": "Dados (.csv)",
+                    "db": "Banco de Dados (.db + .csv)",
+                    "temp": "Salvar para validação",
+                    # "pdf": "Relatório (.pdf)",
+                    # "csv": "Dados (.csv)",
                 }
             },
-            "download_button": "Baixar dados",
+            "download_button": "Salvar dados",
         },
         "logs": {
             "possible_flare": "Possível Flare",
-            "possible_problem": "Possível Problema"
+            "possible_problem": "Possível Problema",
+            "start": "Início",
+            "end": "Fim",
+            "curve": "Curva",
+            "offset": "Offset",
+            "median": "Mediana",
+            "add_log_label": "Adicionar novo evento",
         },
         "graphs": {
             "intensity": "Intensidade",
@@ -89,6 +148,31 @@ TRANSLATIONS = {
             "normalized": "Normalizada",
             "time": "Tempo",
             "curve": "Curva",
+            "smoothed": "Suavizado",
+            "original": "Original",
+            "no_data_found": "Sem dados disponíveis",
+        },
+        "notifications": {
+            "date_not_found": {
+                "title": "Data não encontrada",
+                "message": "A data selecionada não possui dados disponíveis. Por favor, escolha outra data."
+            },
+            "not_enough_curves": {
+                "title": "Não há curvas suficientes",
+                "message": "A data selecionada não possui dados suficientes em dias próximos."
+            }
+        },
+        "validation": {
+            "search_data_button": "Procurar dados",
+            "load_flares_button": "Carregar Flares",
+            "goes_xray_data": "Dados Raio-X do GOES",
+            "stations": {
+                "palehua": "Palehua - RSTN",
+                "sagamore_hill": "Sagamore Hill - RSTN",
+                "san_vito": "San Vito - RSTN",
+                "learmonth": "Learmonth - RSTN",
+                "nobeyama": "Nobeyama - NoRP"
+            }
         }
-    }
+    },
 }
